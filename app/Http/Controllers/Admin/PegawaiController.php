f2778ae78cebec767fbe8e7394a17afd4f528dbc
grenly-del <ardiand400@gmail.com>
2026-07-10 15:09:30 +0800

    fiks:perbaiki error ci

--- app/Http/Controllers/Admin/PegawaiController.php
+++ app/Http/Controllers/Admin/PegawaiController.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Employee\StoreEmployeeRequest;
 use App\Http\Requests\Employee\UpdateEmployeePerformanceFlagRequest;
 use App\Http\Requests\Employee\UpdateEmployeeRequest;
+use App\Http\Requests\Employee\UpdateEmployeeSatyalancanaEligibilityRequest;
 use App\Models\Employee;
 use App\Models\RefAgama;
 use App\Models\RefEselon;
