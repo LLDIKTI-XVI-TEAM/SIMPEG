ecb335e55fd5a114b47f60f14a6159ebdc819a9b
adithiachristian7 <adithiachristian@gmail.com>
2026-07-19 01:04:47 +0800

    Mengubah warna dan icon pada beberapa tombol laporan dan pengajuan cuti

--- resources/views/admin/laporan/export-pegawai.blade.php
+++ resources/views/admin/laporan/export-pegawai.blade.php
@@ -49,9 +49,9 @@
                 {{-- Export Excel --}}
                 <x-ui.button as="a" x-bind:href="exportUrl" variant="secondary">
                     <svg class="w-4 h-4 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                     </svg>
-                    Export Excel
+                    Unduh Excel
                 </x-ui.button>
             </div>
         </div>
