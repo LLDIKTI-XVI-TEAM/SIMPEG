90f14371ebb9249b9e23ce979ea74b6b81f3cbaa
grenly-del <ardiand400@gmail.com>
2026-07-27 18:05:28 +0800

    revert: hapus Turbo JS dari aplikasi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,5 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
-import * as Turbo from "@hotwired/turbo";
 
 window.Alpine = Alpine;
 Alpine.start();
-
-// Tampilkan skeleton loading saat transisi halaman dengan Turbo
-document.addEventListener("turbo:visit", () => {
-    const mainContent = document.getElementById('main-content');
-    const skeleton = document.getElementById('global-skeleton');
-    
-    if (skeleton && mainContent) {
-        mainContent.classList.add('hidden');
-        skeleton.classList.remove('hidden');
-    }
-});
