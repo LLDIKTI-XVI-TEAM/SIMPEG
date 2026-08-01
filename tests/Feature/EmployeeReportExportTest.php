d11fc59a3bef9344c065c7fba3a239e0b0e10c6f
adithiachristian7 <adithiachristian@gmail.com>
2026-08-01 17:39:45 +0800

    fix(export-pegawai): cegah cetak PDF saat pratinjau gagal

--- tests/Feature/EmployeeReportExportTest.php
+++ tests/Feature/EmployeeReportExportTest.php
@@ -190,6 +190,17 @@ class EmployeeReportExportTest extends TestCase
             });
     }
 
+    public function test_pdf_print_is_blocked_when_preview_is_not_current(): void
+    {
+        $admin = User::factory()->adminKepegawaian()->create();
+
+        $this->actingAs($admin)
+            ->get(route('laporan.pegawai'))
+            ->assertOk()
+            ->assertSee('x-bind:disabled="previewLoading || previewError || pensiunError"', false)
+            ->assertSee('if (this.previewLoading || this.previewError || this.pensiunError)', false);
+    }
+
     public function test_preview_applies_initial_row_range_once_on_backend(): void
     {
         $admin = User::factory()->adminKepegawaian()->create();
