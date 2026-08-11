8166e879e874133ea3faac44c9c33339d3b0be76
Diyon18 <dkobi16@gmail.com>
2026-08-12 00:31:56 +0800

    fix: selaraskan tipe tanggal status pada model pegawai
    - Deklarasikan status_tanggal sebagai Carbon nullable sesuai cast Eloquent.
    - Pastikan PHPStan penuh dapat memvalidasi assertion tanggal pada regression test Issue #188.

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Carbon;
  * @property string|null $jenis_pegawai_id
  * @property string|null $status_aktif
  * @property string|null $status_pegawai_id
+ * @property Carbon|null $status_tanggal
  * @property string|null $kepala_bagian_id
  * @property string|null $kelas_jabatan
  * @property string|null $kelas_jabatan_terakhir
